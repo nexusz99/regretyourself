cc039fd8962bf1d909290045c8d64daeca0a9df0
SeongSoo Cho <nexusz99@gmail.com>
2017-12-08 00:17:09 +0900

    휴!

--- regret/models.py
+++ regret/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from regret.database import Base
 
@@ -6,4 +7,17 @@ from regret.database import Base
 class Article(Base):
     __tablename__ = 'articles'
     id = Column(Integer, primary_key=True)
+    author = Column(String(50))
+    msg = Column(String(10240))
+    create_at = Column(DateTime(timezone='Asia/Seoul'))
+    session_id = Column(String(64))
 
+    thumbsup = relationship("ThumbsUp", backref='thumbs')
+
+
+class ThumbsUp(Base):
+    __tablename__ = 'thumbs'
+    id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, ForeignKey(Article.id))
+    session_id = Column(String(64))
+    create_at = Column(DateTime(timezone='Asia/Seoul'))
--- regret/run.py
+++ regret/run.py
@@ -1,9 +1,15 @@
 import random
 
-from flask import Flask, render_template, make_response, request
+from flask import Flask, render_template, make_response, request, redirect
 
-from regret.database import db_session, init_db
+from regret.models import Article, ThumbsUp
+from regret.database import db_session, init_db, engine
 
+from sqlalchemy import and_
+
+from datetime import datetime
+
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -19,12 +25,48 @@ def shutdown_session(exception=None):
 
 @app.route('/', methods=['GET'])
 def index():
-    resp = make_response(render_template('index.html'))
+    ar_page = list()
+    articles = Article.query.all()
+    for a in articles:
+        t = ThumbsUp.query.filter(and_(ThumbsUp.session_id == get_cookie(),
+                                       ThumbsUp.article_id == a.id)).first()
+        z = {'id': a.id, 'author': a.author, 'msg': a.msg,
+             'thumbs': len(a.thumbsup), 'btn': 1 if t is None else 0}
+        ar_page.append(z)
+    resp = make_response(render_template('index.html', articles=ar_page))
     if not get_cookie():
         resp.set_cookie('regret', "%0x" % random.getrandbits(128))
     return resp
 
 
+@app.route('/thumbs/<id>')
+def thumbs(id):
+    session = get_cookie()
+    if session is None:
+        return redirect('/')
+
+    t = ThumbsUp.query.filter(and_(ThumbsUp.session_id == get_cookie(),
+                                   ThumbsUp.article_id == id)).first()
+    if t is not None:
+        return redirect('/')
+
+    t = ThumbsUp(article_id=id, session_id=session,
+                 create_at=datetime.now(timezone('Asia/Seoul')))
+    db_session.add(t)
+    db_session.commit()
+    return redirect('/')
+
+
+@app.route('/post', methods=['POST'])
+def post_article():
+    a = Article(author=request.form['nick'], msg=request.form['msg'],
+                create_at=datetime.now(timezone('Asia/Seoul')),
+                session_id=get_cookie())
+    db_session.add(a)
+    db_session.commit()
+    return redirect('/')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', threaded=True)
     init_db()
